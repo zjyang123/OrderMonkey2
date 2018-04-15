f1f3d993bce30d36f87c30b770f391d68de3eda7
James Chen <jxchen@live.ca>
2018-04-15 17:26:12 -0600

    my house geo location

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -76,8 +76,11 @@ export class WelcomePage {
           this.geoCordLong = resp.coords.longitude;
           this.geoCordLat = resp.coords.latitude;
 
-          const testLat = 50.900444// test cords
-          const testLong = -114.085056// test cords
+          // const testLat = 50.900444// test cords
+          // const testLong = -114.085056// test cords
+
+          const testLat = 50.885800// test cords
+          const testLong = -114.089385// test cords
 
           this.userCommunication.geolocationService(this.geoCordLat, this.geoCordLong, testLat, testLong).then((distance) => {
 
