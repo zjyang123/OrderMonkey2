36362c3f80fcf885dfb2f0b7f5daa28d70a9c1ce
zjyang123 <zjyang@ualberta.ca>
2018-05-03 11:02:27 -0600

    item details changes

--- src/pages/table-detail/table-detail.ts
+++ src/pages/table-detail/table-detail.ts
@@ -16,6 +16,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class TableDetailPage {
   public menu;
 
+  itemDetailArray = [
+    {
+      itemID: 1,
+      itemName: 'Eggs',
+      itemURL: 'https://smittenkitchendotcom.files.wordpress.com/2014/10/the-crispy-egg1.jpg',
+      itemDesc: 'Protein rich delicious snack',
+      menuID: 1,
+      options : ['boiled', 'poached', 'fried' ]
+    },
+    {
+      itemID: 2,
+      itemName: 'Muffins',
+      itemDesc: 'Fibre for poopies',
+      itemURL: 'https://www.craftycookingmama.com/wp-content/uploads/2016/03/040.jpg',
+      menuID: 2,
+      options : ['Chocolate', 'Vanilla', 'Bandana Bred' ]
+    }
+
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.menu = navParams.get('menu');
   }
