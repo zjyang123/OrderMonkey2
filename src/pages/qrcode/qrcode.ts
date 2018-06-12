d79495f1c6e6a33711af13389ef7d739cb58bbf5
zjyang123 <zjyang@ualberta.ca>
2018-06-11 21:53:50 -0600

    what if I did nothing

--- src/pages/qrcode/qrcode.ts
+++ src/pages/qrcode/qrcode.ts
@@ -87,7 +87,7 @@ export class QrcodePage {
                   const clientLongCord = geoCordReturn.split(',')[1]; // Longitude
   
                   this.userCommunication.geolocationService(this.geoCordLat, this.geoCordLong, clientLatCord, clientLongCord).then((distance) => {
-                    if (distance > 120) {
+                    if (distance > 5000) {
                       this.notificationBar.notificationbarTask('Oops! Something went wrong!', 1500, 'bottom');
                       this.navCtrl.pop({ animate: false });
                     } else {
