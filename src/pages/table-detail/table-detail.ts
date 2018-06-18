7c8f0ea8dc5938acd3797a086c0499797f80bb01
Default <default@Defaults-Mac-mini.local>
2018-06-17 21:41:47 -0600

    connect submenu to database

--- src/pages/table-detail/table-detail.ts
+++ src/pages/table-detail/table-detail.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, LoadingController } from 'ionic-angular';
 
-import { Item } from '../../models/item';
-import { Items } from '../../providers/providers';
-
+import { MenuControllerService } from '../../app/service/menu-controller.service';
+import { NotificationBarService } from '../../app/service/notificationbar.service';
 /**
  * Generated class for the TableDetailPage page.
  *
@@ -18,25 +17,58 @@ import { Items } from '../../providers/providers';
 })
 export class TableDetailPage {
   public menu;
-  currentItems: Item[];
+  public subMenuResult;
+  public menuNotEmpty = true;
+  public subMenuItems;
+  public getMenuDetail = {
+    menuID: '',
+    clientID: ''
+  };
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
-    public items: Items,
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    public menuService: MenuControllerService, // This is for food menu controller service
+    public loadingCtrl: LoadingController,
+    public notificationBar: NotificationBarService
+    
   ) {
     this.menu = navParams.get('menu');
-    this.currentItems = this.items.query();
+    this.getMenuDetail.clientID = this.menu.client_id;
+    this.getMenuDetail.menuID = this.menu.id;
+    this.subMenuDetails();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TableDetailPage');
+  subMenuDetails() {
+    let loading = this.loadingCtrl.create({
+      content: ''
+    });
+    loading.present();
+    this.menuService.getSubMenu(this.getMenuDetail).then((val) => {
+      this.subMenuResult = val;
+      this.menuNotEmpty = this.subMenuResult.subMenuSet;
+      if (this.menuNotEmpty) {
+        this.subMenuItems = this.subMenuResult.result;
+      }
+      loading.dismiss();
+    }, (err) => {
+      loading.dismiss();
+    });
   }
 
   itemDetails(item:any) {
+    if (item.has_options == 1) {
+      const message = 'Item added to your cart!';
+      this.notificationBar.notificationbarTask(message, 3000, 'bottom');
+    } else {
+      let profileModal = this.modalCtrl.create('ItemDetailPage', {item: item});
+      profileModal.present();
+    }
+  }
+
+  pressItemDetails(item:any) {
     let profileModal = this.modalCtrl.create('ItemDetailPage', {item: item});
     profileModal.present();
   }
-
 }
